ec3f0892a418439f51b1eae01b6a8954a0b77913
nequamy <nequamy@gmail.com>
2025-09-25 17:59:59 +0300

    [~] add ascii arts for score screen

--- src/terminake/states/score_state.py
+++ src/terminake/states/score_state.py
@@ -6,6 +6,95 @@ from states.base_state import BaseState, GameStatesEnum
 
 
 class ScoreState(BaseState):
+    _score_art: list = [
+        "███████╗ ██████╗ ██████╗ ██████╗ ███████╗",
+        "██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝",
+        "███████╗██║     ██║   ██║██████╔╝█████╗",
+        "╚════██║██║     ██║   ██║██╔══██╗██╔══╝",
+        "███████║╚██████╗╚██████╔╝██║  ██║███████╗",
+        "╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝",
+    ]
+
+    _0_art: list = [
+        "  ██████╗  ",
+        " ██╔═████╗ ",
+        " ██║██╔██║ ",
+        " ████╔╝██║ ",
+        " ╚██████╔╝ ",
+        "  ╚═════╝  ",
+    ]
+    _1_art: list = ["  ██╗ ", " ███║ ", " ╚██║ ", "  ██║ ", "  ██║ ", "  ██║ "]
+    _2_art: list = [
+        " ██████╗  ",
+        " ╚════██╗ ",
+        " █████╔╝  ",
+        " ██╔═══╝  ",
+        " ███████╗ ",
+        " ╚══════╝ ",
+    ]
+    _3_art: list = [
+        " ██████╗  ",
+        " ╚════██╗ ",
+        " █████╔╝  ",
+        " ╚═══██╗  ",
+        " ██████╔╝ ",
+        " ╚═════╝  ",
+    ]
+    _4_art: list = [
+        " ██╗  ██╗ ",
+        " ██║  ██║ ",
+        " ███████║ ",
+        " ╚════██║ ",
+        "      ██║ ",
+        "      ╚═╝ ",
+    ]
+    _5_art: list = [
+        " ███████╗ ",
+        " ██╔════╝ ",
+        " ███████╗ ",
+        " ╚════██║ ",
+        " ███████║ ",
+        " ╚══════╝ ",
+    ]
+    _6_art: list = [
+        "  ██████╗  ",
+        " ██╔════╝  ",
+        " ███████╗  ",
+        " ██╔═══██╗ ",
+        " ╚██████╔╝ ",
+        "  ╚═════╝  ",
+    ]
+    _7_art: list = [
+        " ███████╗ ",
+        " ╚════██║ ",
+        "    ██╔╝  ",
+        "   ██╔╝   ",
+        "  ██║     ",
+        "  ╚═╝     ",
+    ]
+    _8_art: list = [
+        "  █████╗  ",
+        " ██╔══██╗ ",
+        " ╚█████╔╝ ",
+        " ██╔══██╗ ",
+        " ╚█████╔╝ ",
+        "  ╚════╝  ",
+    ]
+    _9_art: list = [
+        "  █████╗  ",
+        " ██╔══██╗ ",
+        " ╚██████║ ",
+        "  ╚═══██║ ",
+        "  █████╔╝ ",
+        "  ╚════╝  ",
+    ]
+
+    _hint_art: list = [
+        "░█▀█░█▀▄░█▀▀░█▀▀░█▀▀░░░▄▀▄░░░█▀█░█▀▄░░░▄▀▄░░░█▀▀░█▀█░█▀▄░░░█▄█░█▀▀░█▀█░█░█",
+        "░█▀▀░█▀▄░█▀▀░▀▀█░▀▀█░░░█\█░░░█░█░█▀▄░░░█\█░░░█▀▀░█░█░█▀▄░░░█░█░█▀▀░█░█░█░█",
+        "░▀░░░▀░▀░▀▀▀░▀▀▀░▀▀▀░░░░▀\░░░▀▀▀░▀░▀░░░░▀\░░░▀░░░▀▀▀░▀░▀░░░▀░▀░▀▀▀░▀░▀░▀▀▀",
+    ]
+
     def handle_input(self, key: int):
         if key == ord("q") or key == ord("Q"):
             return GameStatesEnum.MENU
@@ -19,5 +108,22 @@ class ScoreState(BaseState):
 
         max_y, max_x = window.getmaxyx()
 
-        window.addstr(10, 10, "SCORE: {score}".format(score=self.context.score))
-        window.addstr(11, 10, "Press Q or q to out")
+        # ASCII Score Logo Output
+        pos_x: int = (max_x - len(self._score_art[0])) // 2
+        pos_y: int = (max_y // 3) - len(self._score_art)
+        for i in range(len(self._score_art)):
+            pos_y += 1
+            window.addstr(pos_y, pos_x, self._score_art[i], curses.A_BOLD)
+
+        score_str = str(self.context.score)
+        pos_y += 3
+        for i in range(len(score_str)):
+            num = self.__getattribute__("_{key}_art".format(key=score_str[i]))
+            pos_x = (max_x - len(num[0])) // 2
+            for line_idx, line in enumerate(num):
+                window.addstr(pos_y + line_idx, pos_x, line, curses.A_BOLD)
+
+        pos_y += 10
+        pos_x = (max_x - len(self._hint_art[0])) // 2
+        for i in range(len(self._hint_art)):
+            window.addstr(pos_y + i, pos_x, self._hint_art[i], curses.A_BOLD)
